77c6b492c6c200a38356dc48c28eb5f458484f2a
eelke <eelkevanderbij.prive+github@gmail.com>
2021-02-18 16:06:31 -0300

    Update seng generator template

--- build-tools/generator-template/block/{name_sc}/{name_pc}.ts
+++ build-tools/generator-template/block/{name_sc}/{name_pc}.ts
@@ -1,10 +1,15 @@
 import AbstractBlock from '../AbstractBlock';
+// import { scrollTransition, baseTransition } from '../../../util/transitions';
+
 
 export default class {{name_pc}} extends AbstractBlock {
   public static readonly displayName:string = '{{name_sc}}';
 
   constructor(el:HTMLElement) {
     super(el);
+
+    // const transitionItems = this.getElements('[data-transition-item]');
+    // scrollTransition(el, baseTransition(transitionItems), { markers: true });
   }
 
   public dispose() {
