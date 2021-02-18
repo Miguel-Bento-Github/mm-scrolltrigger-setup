f5123dc1223264761320ddd3b943e24464b33ceb
eelke <eelkevanderbij.prive+github@gmail.com>
2021-02-18 16:06:16 -0300

    Add transitions

--- src/app/component/block/banner/Banner.ts
+++ src/app/component/block/banner/Banner.ts
@@ -1,10 +1,13 @@
 import AbstractBlock from '../AbstractBlock';
+import { scrollTransition, visualTransition } from '../../../util/transitions';
 
 export default class Banner extends AbstractBlock {
   public static readonly displayName: string = 'banner';
 
   constructor(el: HTMLElement) {
     super(el);
+    const visual = this.getElement('[data-component="visual"]');
+    visual && scrollTransition(el, visualTransition(visual));
   }
 
   public dispose() {
--- src/app/component/block/text-block/TextBlock.ts
+++ src/app/component/block/text-block/TextBlock.ts
@@ -1,10 +1,23 @@
+import { gsap } from 'gsap';
 import AbstractBlock from '../AbstractBlock';
+import { scrollTransition, baseTransition, visualTransition } from '../../../util/transitions';
 
 export default class TextBlock extends AbstractBlock {
   public static readonly displayName: string = 'text-block';
 
   constructor(el: HTMLElement) {
     super(el);
+
+    const transitionItems = this.getElements('[data-transition-item]');
+
+    const visuals = this.getElements('[data-component="visual"]');
+
+    const animation = gsap
+      .timeline()
+      .add(baseTransition(transitionItems))
+      .add(visualTransition(visuals));
+
+    scrollTransition(el, animation);
   }
 
   public dispose() {
